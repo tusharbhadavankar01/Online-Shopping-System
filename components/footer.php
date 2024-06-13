331d2d1fbc6e66c1d4afe6657936aeab74057d13
tusharbhadavankar01 <143948264+tusharbhadavankar01@users.noreply.github.com>
2024-06-13 20:41:04 +0530

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,41 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>Quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> Home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> About</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> Shop</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> Contact</a>
+      </div>
+
+      <div class="box">
+         <h3>Extra links</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> Login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> Register</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> Cart</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> Orders</a>
+      </div>
+
+      <div class="box">
+         <h3>Contact Us.</h3>
+         <a href="tel:9800000000"><i class="fas fa-phone"></i> 1234567890</a>
+         <a href="tel:9900000000"><i class="fas fa-phone"></i> 8466789260</a>
+         <a href="mailto:onlineshop@gmail.com"><i class="fas fa-envelope"></i>onlineshop@gmail.com</a>
+         <a href="https://www.google.com/myplace"><i class="fas fa-map-marker-alt"></i> India </a>
+      </div>
+
+      <div class="box">
+         <h3>Follow Us</h3>
+         <a href="#" target="_blank"><i class="fab fa-facebook-f"></i>facebook</a>
+         <a href="#" target="_blank"><i class="fab fa-twitter"></i>Twitter</a>
+         <a href="#" target="_blank"><i class="fab fa-instagram"></i>Instagram</a>
+         <a href="#" target="_blank"><i class="fab fa-linkedin"></i>Linkedin</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Online Shopping System</span> | all rights reserved!</div>
+
+</footer>
